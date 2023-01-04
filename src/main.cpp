bf3b88ed9155d7bcf366a5214162c3ecdfa072b8
Ray <jrchang11015043@gmail.com>
2023-01-05 04:39:59 +0800

    npr?

--- src/main.cpp
+++ src/main.cpp
@@ -18,8 +18,6 @@ int main() {
     //GameWindow win(sf::VideoMode(1200, 900), "SFML window");
     sf::RenderWindow final_window(sf::VideoMode(1200, 900), "final");
     GameWindow win(sf::VideoMode(1200, 900), "SFML window", &final_window);
-
-//    std::thread listen(EatInput);
 //    listen.detach();
     win.run();
 //    str = "end";
